a5080ac3d8c99ddad720d809e3247a9f6d6e620d
Villers Mickael <villersm@hotmail.com>
2014-02-28 22:53:52 +0100

    Modification de la webview

--- spi0n/src/main/java/com/ghota/spi0n/PostActivity.java
+++ spi0n/src/main/java/com/ghota/spi0n/PostActivity.java
@@ -1,176 +1,82 @@
 package com.ghota.spi0n;
 
-import android.content.res.Configuration;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.WindowManager;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.FrameLayout;
-import android.widget.LinearLayout;
 
 import com.ghota.spi0n.model.PostData;
 
 public class PostActivity extends ActionBarActivity {
+    private WebView webView;
+    private FrameLayout customViewContainer;
+    private WebChromeClient.CustomViewCallback customViewCallback;
+    private View mCustomView;
+    private myWebChromeClient mWebChromeClient;
+    private myWebViewClient mWebViewClient;
 
-    protected PostData post;
-    protected FrameLayout webViewPlaceholder;
-    protected WebView webView;
-
+    /**
+     * Called when the activity is first created.
+     */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
 
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        // recupere le Bundle
-        post = getIntent().getExtras().getParcelable("post");
-
+        PostData post = getIntent().getExtras().getParcelable("post");
         setTitle(post.postTitle);
 
+        customViewContainer = (FrameLayout) findViewById(R.id.customViewContainer);
+        webView = (WebView) findViewById(R.id.webView);
 
-        // Initialize l'UI
-        initUI();
+        mWebViewClient = new myWebViewClient();
+        webView.setWebViewClient(mWebViewClient);
+
+        mWebChromeClient = new myWebChromeClient();
+        webView.setWebChromeClient(mWebChromeClient);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setAppCacheEnabled(true);
+        webView.getSettings().setBuiltInZoomControls(true);
+        webView.getSettings().setSaveFormData(true);
+        webView.loadUrl(AppConstants.URL_ARTICLE_HTML+post.postGuid);
     }
 
-    private void initUI() {
-        webViewPlaceholder = ((FrameLayout)findViewById(R.id.webViewPlaceholder));
+    public boolean inCustomView() {
+        return (mCustomView != null);
+    }
 
-        // Initialize the WebView if necessary
-        if (webView == null)
-        {
-            webView = new WebView(this);
-            webView.setLayoutParams(new ViewGroup.LayoutParams(FrameLayout.LayoutParams.FILL_PARENT, FrameLayout.LayoutParams.FILL_PARENT));
-            webView.getSettings().setSupportZoom(true);
-            webView.getSettings().setBuiltInZoomControls(true);
-            webView.getSettings().setJavaScriptEnabled(true);
-            webView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
-            webView.setScrollbarFadingEnabled(true);
+    public void hideCustomView() {
+        mWebChromeClient.onHideCustomView();
+    }
 
-            webView.setWebViewClient(new WebViewClient() {
-                public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                    view.loadUrl(url);
-                    return true;
-                }
-            });
+    @Override
+    protected void onPause() {
+        super.onPause();    //To change body of overridden methods use File | Settings | File Templates.
+        webView.onPause();
+    }
 
-            webView.setWebChromeClient(new WebChromeClient() {
+    @Override
+    protected void onResume() {
+        super.onResume();    //To change body of overridden methods use File | Settings | File Templates.
+        webView.onResume();
+    }
 
-                FrameLayout.LayoutParams LayoutParameters = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT);
-                public View mCustomView;
-                public LinearLayout mContentView;
-                public FrameLayout mCustomViewContainer;
-                public CustomViewCallback mCustomViewCallback;
-
-                @Override
-                public void onShowCustomView(View view, CustomViewCallback callback) {
-
-                    if (mCustomView != null) {
-                        callback.onCustomViewHidden();
-                        return;
-                    }
-                    mContentView = (LinearLayout) findViewById(R.id.LinearLayoutPost);
-                    mContentView.setVisibility(View.GONE);
-                    mCustomViewContainer = new FrameLayout(PostActivity.this);
-                    mCustomViewContainer.setLayoutParams(LayoutParameters);
-                    mCustomViewContainer.setBackgroundResource(android.R.color.black);
-                    view.setLayoutParams(LayoutParameters);
-                    mCustomViewContainer.addView(view);
-                    mCustomView = view;
-                    mCustomViewCallback = callback;
-                    mCustomViewContainer.setVisibility(View.VISIBLE);
-                    setContentView(mCustomViewContainer);
-                }
-
-                @Override
-                public void onHideCustomView() {
-                    super.onHideCustomView();
-                    if (mCustomView == null) {
-                        return;
-                    } else {
-                        // Hide the custom view.
-                        mCustomView.setVisibility(View.GONE);
-                        // Remove the custom view from its container.
-                        mCustomViewContainer.removeView(mCustomView);
-                        mCustomView = null;
-                        mCustomViewContainer.setVisibility(View.GONE);
-                        mCustomViewCallback.onCustomViewHidden();
-                        // Show the content view.
-                        mContentView.setVisibility(View.VISIBLE);
-
-                        setContentView(mContentView);
-                    }
-                }
-            });
-
-            webView.loadUrl(AppConstants.URL_ARTICLE_HTML+post.postGuid);
+    @Override
+    protected void onStop() {
+        super.onStop();    //To change body of overridden methods use File | Settings | File Templates.
+        if (inCustomView()) {
+            hideCustomView();
         }
-
-        webViewPlaceholder.addView(webView);
-    }
-
-    @Override
-    public void onConfigurationChanged(Configuration newConfig){
-        if (webView != null)
-        {
-            // Remove the WebView from the old placeholder
-            webViewPlaceholder.removeView(webView);
-        }
-
-        super.onConfigurationChanged(newConfig);
-
-        // Load the layout resource for the new configuration
-        setContentView(R.layout.activity_post);
-
-        // Reinitialize the UI
-        initUI();
-    }
-
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-
-        // Save the state of the WebView
-        webView.saveState(outState);
-    }
-
-    @Override
-    protected void onRestoreInstanceState(Bundle savedInstanceState) {
-        super.onRestoreInstanceState(savedInstanceState);
-
-        // Restore the state of the WebView
-        webView.restoreState(savedInstanceState);
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        toggleWebViewState(true);
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        toggleWebViewState(false);
-    }
-
-    private void toggleWebViewState(boolean pause){
-        try
-        {
-            Class.forName("android.webkit.WebView")
-                    .getMethod(pause
-                            ? "onPause"
-                            : "onResume", (Class[]) null)
-                    .invoke(webView, (Object[]) null);
-        }
-        catch (Exception e){}
     }
 
     @Override
@@ -184,6 +90,9 @@ public class PostActivity extends ActionBarActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case android.R.id.home:
+                if (inCustomView()) {
+                    hideCustomView();
+                }
                 finish();
                 break;
 
@@ -192,4 +101,83 @@ public class PostActivity extends ActionBarActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+
+            if (inCustomView()) {
+                hideCustomView();
+                return true;
+            }
+
+            if ((mCustomView == null) && webView.canGoBack()) {
+                webView.goBack();
+                return true;
+            }
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    class myWebChromeClient extends WebChromeClient {
+        private Bitmap mDefaultVideoPoster;
+        private View mVideoProgressView;
+
+        @Override
+        public void onShowCustomView(View view, int requestedOrientation, CustomViewCallback callback) {
+            onShowCustomView(view, callback);    //To change body of overridden methods use File | Settings | File Templates.
+        }
+
+        @Override
+        public void onShowCustomView(View view,CustomViewCallback callback) {
+
+            // if a view already exists then immediately terminate the new one
+            if (mCustomView != null) {
+                callback.onCustomViewHidden();
+                return;
+            }
+            mCustomView = view;
+            webView.setVisibility(View.GONE);
+            customViewContainer.setVisibility(View.VISIBLE);
+            customViewContainer.addView(view);
+            customViewCallback = callback;
+        }
+
+        @Override
+        public View getVideoLoadingProgressView() {
+
+            if (mVideoProgressView == null) {
+                LayoutInflater inflater = LayoutInflater.from(PostActivity.this);
+                mVideoProgressView = inflater.inflate(R.layout.video_progress, null);
+            }
+            return mVideoProgressView;
+        }
+
+        @Override
+        public void onHideCustomView() {
+            super.onHideCustomView();    //To change body of overridden methods use File | Settings | File Templates.
+            if (mCustomView == null)
+                return;
+
+            webView.setVisibility(View.VISIBLE);
+            customViewContainer.setVisibility(View.GONE);
+
+            // Hide the custom view.
+            mCustomView.setVisibility(View.GONE);
+
+            // Remove the custom view from its container.
+            customViewContainer.removeView(mCustomView);
+            customViewCallback.onCustomViewHidden();
+
+            mCustomView = null;
+        }
+    }
+
+    class myWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            return super.shouldOverrideUrlLoading(view, url);    //To change body of overridden methods use File | Settings | File Templates.
+        }
+    }
+
 }
